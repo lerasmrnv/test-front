5cf14b5c83ea2aeefcee9accd9bcb60e201bc265
lerasmrnv <lera.smirnova.flok@gmail.com>
2023-05-22 14:00:16 +0300

    Add a new function

---
+++ scrips/timer.js
@@ -0,0 +1,40 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const deadline = new Date (2023, 04, 31)
+    let timerId = null;
+    
+    function declensionNum(num, words) {
+    return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+    }
+
+    function countdownTimer() {
+        const diff = deadline - new Date();
+        if (diff <= 0) {
+            clearInterval(timerId);
+        }
+        const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+        const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+        const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+        const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+
+        $days.textContent = days < 10 ? '0' + days : days;
+        $hours.textContent = hours < 10 ? '0' + hours : hours;
+        $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+        $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+
+        $days.dataset.title = declensionNum(days, ['день', 'дня', 'дней']);
+        $hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
+        $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+        $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+    }
+
+    const $days = document.querySelector('.frame-timer_days-count');
+    const $hours = document.querySelector('.frame-timer_hours-count');
+    const $minutes = document.querySelector('.frame-timer_minutes-count');
+    const $seconds = document.querySelector('.frame-timer_seconts-count');
+
+    countdownTimer();
+
+    timerId = setInterval(countdownTimer, 1000);
+})
